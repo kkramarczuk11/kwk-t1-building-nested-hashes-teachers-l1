347808cbed266dffe8f0f83236c94cbd8f5a8718
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-10 16:01:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague=>{
-       :patriach=>{},
-       :matriarch=>{},
-       :hero=>{},
-       :hero_friends=>{}
-     },
-     :capulet=>{:patriach=>{},
-     :matriarch=>{},
-     :hero=>{},
-     :hero_friends=>{}
-   }
+     :montague=>{},
+     :capulet=>{}
    }
 end
